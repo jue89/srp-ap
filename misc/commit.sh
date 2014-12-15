86cb41056fa58464e0a9cc76c4556d16e1e8404b
Jue <me@jue.yt>
2014-12-15 05:41:00 +0100

    Adjusted scripts to fresh OpenWrt image

--- misc/commit.sh
+++ misc/commit.sh
@@ -1,9 +1,56 @@
 #!/bin/sh
 
-echo $IPV6_ADDR
-echo $AAA_SECRET
-echo $AAA_FQDN
-echo $AAA_IPV6
-echo $AAA_PUBLIC_KEY
+# Get private key and ID
+PRIVATE_KEY=$(grep "\"privateKey\"" /etc/api/localConf.json | awk -F\" '{print $4}')
+ID=$(grep "\"id\"" /etc/api/localConf.json | awk -F\" '{print $4}')
+
+# Set fastd configuration
+uci set fastd.mgmt=fastd
+uci set fastd.mgmt.enabled=1
+uci set fastd.mgmt.syslog_level=info
+uci set fastd.mgmt.mode=tap
+uci set fastd.mgmt.interface=fastd0
+uci set fastd.mgmt.mtu=1426
+uci set fastd.mgmt.forward=0
+uci set fastd.mgmt.secure_handshakes=1
+uci set fastd.mgmt.secret=$PRIVATE_KEY
+uci add_list fastd.mgmt.method=xsalsa20-poly1305
+uci set fastd.aaa=peer
+uci set fastd.aaa.enabled=1
+uci set fastd.aaa.net=mgmt
+uci set fastd.aaa.key=$AAA_PUBLIC_KEY
+uci add_list fastd.aaa.remote=\"$AAA_FQDN\"\ port\ 1337
+uci set network.mgmt=interface
+uci set network.mgmt.proto=static
+uci set network.mgmt.ifname=fastd0
+uci set network.mgmt.ip6addr=$IPV6_ADDR/64
+
+# Remove default Wi-Fi network
+if uci show wireless | grep -q "ssid=OpenWrt"
+then
+	uci delete $(uci show wireless | grep "ssid=OpenWrt" | awk -F. '{print $1 "." $2}')
+fi
+
+# Set Wi-Fi configuration
+IFACE=$(uci add wireless wifi-iface)
+uci set wireless.$IFACE.device=radio0
+uci set wireless.$IFACE.mode=ap
+uci set wireless.$IFACE.wmm=0
+uci set wireless.$IFACE.encryption=wpa2+ccmp
+uci set wireless.$IFACE.ssid=13pm.eu
+uci set wireless.$IFACE.network=lan
+uci set wireless.$IFACE.auth_server=$AAA_IPV6
+uci set wireless.$IFACE.auth_secret=$AAA_SECRET
+uci set wireless.$IFACE.acct_server=$AAA_IPV6
+uci set wireless.$IFACE.acct_secret=$AAA_SECRET
+uci set wireless.$IFACE.nasid=$ID
+uci set wireless.radio0.disabled=0
+
+# And here we go
+uci commit fastd
+uci commit wireless
+uci commit network
+/etc/init.d/fastd restart
+/etc/init.d/network restart
 
 exit 0
--- misc/revert.sh
+++ misc/revert.sh
@@ -1,3 +1,43 @@
 #!/bin/sh
 
+RESTART=0
+
+# Remove all existing configuration
+
+if uci show fastd | grep -q ".mgmt"
+then
+	uci delete fastd.mgmt
+	RESTART=1
+fi
+
+if uci show fastd | grep -q ".aaa"
+then
+	uci delete fastd.aaa
+	RESTART=1
+fi
+
+if uci show network | grep -q ".mgmt"
+then
+	uci delete network.mgmt
+	RESTART=1
+fi
+
+if uci show wireless | grep -q "ssid=13pm.eu"
+then
+	uci delete $(uci show wireless | grep "ssid=13pm.eu" | awk -F. '{print $1 "." $2}')
+fi
+
+
+
+# And commit it
+
+if test "$RESTART" == "1"
+then
+	uci commit fastd
+	uci commit network
+	uci commit wireless
+	/etc/init.d/fastd restart
+	/etc/init.d/network restart
+fi
+
 exit 0
